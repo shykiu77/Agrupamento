2f3726f0df60c6e95d0d8a1171fbddb58c58d6fc
shykiu77 <carlosgabrielsns@gmail.com>
2019-09-18 05:01:33 -0300

    renomeei o header.

--- ensemble.h
+++ ensemble.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MALLOC(t,n) (t*)malloc(sizeof(t)*n)
 #define CALLOC(t,n) (t*)calloc(sizeof(t),n)
