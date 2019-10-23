8981d7c832c1e90f9fc1f3c2685f33d53caa6362
shykiu77 <carlosgabrielsns@gmail.com>
2019-10-23 15:42:23 -0300

    Adição do calculo das métricas internas.

--- autoMetricas.sh
+++ autoMetricas.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#args: local dos arquivos, numero de elementos, local do cluster verdadeiro, local da saida do script.
-#Ex.: ./autoMetricasExternas.sh Saidas/Wine/ 178 Entradas/Wine/true_clusters_wine.txt Resultados/wineTeste
+#args: local dos arquivos, numero de elementos, local do cluster verdadeiro, local da saida do script, numero de atributos e arquivo do dataset.
+#Ex.: ./autoMetricas.sh Saidas/Subcorel2/Subcorel2_5/  400 Entradas/Subcorel2/true_clustering  Resultados/subcorel2_5 338 Entradas/Subcorel2/subcorel2_attributes
 
 for filename in $1*; do
     echo $filename >> $4 
     Bin/metricasExternas $2 $3 $filename >> $4
+    Bin/metricasInternas $2 $5 $6 $filename >> $4
+
     echo "" >> $4
 done
