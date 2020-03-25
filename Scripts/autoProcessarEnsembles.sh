f1397233a93a888229718a79c22e4bd2c15127a1
shykiu77 <carlosgabrielsns@gmail.com>
2020-03-25 02:03:55 -0300

    Backup

--- Scripts/autoProcessarEnsembles.sh
+++ Scripts/autoProcessarEnsembles.sh
@@ -3,11 +3,11 @@
 # local dos binarios, pasta que contem os arquivos de entrada, numero de partições, local de saida dos algoritmos, true cluster, dataset,  Nelementos, Natributos, local de saida das metricas
 
 for clustering in $2*; do
-    folderName=$4"$(echo $clustering | cut -d'/' -f5)"
-    mkdir $folderName
-    ./Scripts/autoAlgoritmos.sh $1 $clustering $3 $folderName/result
-    rm $folderName/*CSPA
-    rm $folderName/*tempFile*
+    #folderName=$4"$(echo $clustering | cut -d'/' -f5)"
+    #mkdir $folderName
+    #./Scripts/autoAlgoritmos.sh $1 $clustering $3 $folderName/result
+    #rm $folderName/*CSPA
+    #rm $folderName/*tempFile*
     Natributos=$(<$8)
     Bin/metricasEnsemble $clustering $5 $6 $Natributos $9/"$(echo $clustering | cut -d'/' -f5)""_medio"
 done
